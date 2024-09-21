53263f96831a7597f98996ccd6372214b671ce1a
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-09-21 11:53:33 +0900

    release: aws RDS 호스트로 변경

--- src/database/connect/mariadb.ts
+++ src/database/connect/mariadb.ts
@@ -1,10 +1,10 @@
 import mariadb from "mysql";
 const conn = mariadb.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "Blog",
+    host: process.env.DB_HOST,
+    port: +(process.env.DB_PORT || 0),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 
 export default conn;
